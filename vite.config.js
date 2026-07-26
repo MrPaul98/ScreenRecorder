ed1fb594adafea353f22bc69ad853f2b31480f70
MrPaul98 <sayandippaul1998@gmail.com>
2026-07-26 21:50:52 +0530

    deploy app test 1

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/screen_recorder/',
+  base: '/screen-recorder/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
